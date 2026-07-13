7df38fce061a12f736922df1e518f9664b9f8bc1
Arpit Khandelwal <masterarpit@gmail.com>
2026-07-13 22:34:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,97 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
-function App() {
 
-    const [count, setCount] = useState(10);
-
-    function increment() {
-        setCount(count + 1);
+function ChatInput({ chatMessages, setChatMessages }) {
+    const [inputText, setInputText] = useState('');
+    function saveInputText(event) {
+        setInputText(event.target.value);
     }
 
-    function decrement() {
-        setCount(count - 1);
+    function sendMessage() {
+        const response = Chatbot.getResponse(inputText);
+        setChatMessages([...chatMessages,
+        {
+            message: inputText,
+            sender: "user",
+            id: crypto.randomUUID()
+        },
+        {
+            message: response,
+            sender: "robot",
+            id: crypto.randomUUID()
+        }]);
+
+        setInputText('');
     }
 
     return (
-        <div className="App">
-            <h2>Count = {count}</h2>
+        <div className="chat-input-container">
+            <input type="text"
+                placeholder="Send a message to chatbot"
+                size="30"
+                onChange={saveInputText}
+                value={inputText}
+                className="chat-input"
+            />
+            <button onClick={sendMessage} className="send-button">Send</button>
+        </div>
+    );
+}
 
-            <button className="btn" onClick={increment}>
-                Increment
-            </button>
+function ChatMessage({ message, sender }) {
+    return (
+        <div className={sender === 'robot' ? 'chat-message-robot' : 'chat-message-user'}>
+            {sender === 'robot' && (
+                <>
+                    <img src="../robot.png" className="chat-message-profile" />
+                    &nbsp;
+                </>
+            )}
+            <div className="chat-message-text">
+                {message}
+            </div>
+            {sender === 'user' && (
+                <>
+                    &nbsp;
+                    <img src="../user.png" className="chat-message-profile" />
+                </>
+            )}
+        </div>
+    );
+}
 
-            {" "}
+function ChatMessages({ chatMessages }) {
+    const chatMessagesRef = useRef(null);
+    useEffect(() => {
+        const containerElem = chatMessagesRef.current;
+        if (containerElem) {
+            containerElem.scrollTop = containerElem.scrollHeight;
+        }
+    }, [chatMessages]);
 
-            <button className="btn" onClick={decrement}>
-                Decrement
-            </button>
+    return (
+        <div className="chat-messages-container" ref={chatMessagesRef}>
+            {chatMessages.map((chatMessage) => {
+                return (
+                    <ChatMessage message={chatMessage.message}
+                        sender={chatMessage.sender}
+                        key={chatMessage.id} />
+                )
+            })
+            }
+        </div>
+    );
+}
+
+function App() {
+
+    const [chatMessages, setChatMessages] = useState([]);
+
+    return (
+        <div className="app-container">
+
+            <ChatMessages chatMessages={chatMessages} />
+            <ChatInput chatMessages={chatMessages} setChatMessages={setChatMessages} />
         </div>
     );
 }
--- src/test.js
+++
@@ -1,5 +0,0 @@
-import http from "http";
-
-http.createServer((req, res) => {
-    res.end("Hello");
-}).listen(3000);
